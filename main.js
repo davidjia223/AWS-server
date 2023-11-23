2190aef66b7a7daa8da3bd3e8f12a1e039694fb8
David Jia <davidjia223@gmail.com>
2023-11-22 19:28:24 -0800

    Add hamburger menu functionality and responsive navigation

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
 //main.js
 
-document.querySelector('.hamburger-menu').addEventListener('click', function() {
-    document.querySelector('nav').classList.toggle('visible');
-  });
-  
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburger = document.querySelector('.hamburger-menu');
+    const navUL = document.querySelector('nav ul');
+
+    // Toggle menu on hamburger click
+    hamburger.addEventListener('click', function() {
+        navUL.classList.toggle('active');
+    });
+
+    // Close the menu when a link is clicked
+    navUL.addEventListener('click', function(e) {
+        if (e.target.tagName === 'A') {
+            navUL.classList.remove('active');
+        }
+    });
+
+    // Close the menu when clicking outside
+    document.addEventListener('click', function(e) {
+        if (!hamburger.contains(e.target) && !navUL.contains(e.target)) {
+            navUL.classList.remove('active');
+        }
+    });
+});
