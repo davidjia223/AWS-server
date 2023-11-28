d351d9f5be7ba530ea03d5954b56e9227c36fd22
David Jia <davidjia223@gmail.com>
2023-11-27 23:42:53 -0800

    hamburger menu

--- main.js
+++ main.js
@@ -1,25 +1,13 @@
-//main.js
-
 document.addEventListener('DOMContentLoaded', function() {
     const hamburger = document.querySelector('.hamburger-menu');
     const navUL = document.querySelector('nav ul');
 
-    // Toggle menu on hamburger click
-    hamburger.addEventListener('click', function() {
+    function toggleMenu() {
         navUL.classList.toggle('active');
-    });
+    }
 
-    // Close the menu when a link is clicked
-    navUL.addEventListener('click', function(e) {
-        if (e.target.tagName === 'A') {
-            navUL.classList.remove('active');
-        }
-    });
+    
 
-    // Close the menu when clicking outside
-    document.addEventListener('click', function(e) {
-        if (!hamburger.contains(e.target) && !navUL.contains(e.target)) {
-            navUL.classList.remove('active');
-        }
-    });
+    hamburger.addEventListener('click', toggleMenu);
+    document.addEventListener('click', closeMenu);
 });
