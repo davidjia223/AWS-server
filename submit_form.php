f0e91cb83e8d50d6c5876f2c7297806447ca7948
David Jia <davidjia223@gmail.com>
2023-11-28 16:17:42 -0800

    Add thank you modal to form

---
+++ submit_form.php
@@ -0,0 +1,24 @@
+<?php
+// Check if the form is submitted
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Collect and sanitize POST data
+    $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING) ?? "";
+    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL) ?? "";
+    $message = filter_input(INPUT_POST, 'message', FILTER_SANITIZE_STRING) ?? "";
+
+    // Validate email
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "Invalid email format";
+        exit();
+    }
+
+    // Process your data here: send email, store in database, etc.
+
+    // Redirect to a thank you page or display a success message
+    header('Location: thank_you.html');
+    exit();
+} else {
+    // Not a POST request, display a generic error or redirect
+    echo "Error: Invalid request";
+}
+?>
