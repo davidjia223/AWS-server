014abdd8967ab917800fd434cb4ec9233ab56bd2
David Jia <davidjia223@gmail.com>
2023-11-29 16:29:17 -0800

    Hamburger menu animated

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
+//main.js
 document.addEventListener('DOMContentLoaded', function() {
     const hamburger = document.querySelector('.hamburger-menu');
     const navUL = document.querySelector('nav ul');
 
     hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
         navUL.classList.toggle('active');
     });
 
     document.addEventListener('click', event => {
         if (event.target.tagName === 'A' || !hamburger.contains(event.target) && !navUL.contains(event.target)) {
             navUL.classList.remove('active');
+            hamburger.classList.remove('active');
         }
     });
 
